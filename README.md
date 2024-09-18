e746eb7f7bf3e931c927afeb5efade358d85d3aa
Diego de Souza Santana <diegossantana068@gmail.com>
2024-09-18 14:59:47 -0300

    README

--- README.md
+++ README.md
@@ -1,20 +1,80 @@
-# Welcome to Docker
+# Desenvolvimento Back-end Avançado (Front-end com React)
+  &nbsp;
 
-This is a repo for new users getting started with Docker.
+## GarageLink
 
-You can try it out using the following command.
+O objetivo deste projeto é desenvolver um site de compra e venda de carros que facilite a comunicação direta entre vendedores e compradores. Inspirado em plataformas como OLX e Webmotors, o site não realiza transações, mas permite a interação entre usuários em nível global.
+    &nbsp;
+
+---
+## Projeto Front-end com React e TailwindCSS
+
+Projeto feito com 9 rotas completamente funcionais.
+
+As rotas utilizadas no projeto são: 
+
+* Página de cadastro do usuário: `("/cadastrar")`
+* Página de login do usuário: `("/entrar")`
+* Menu principal: `("/")`
+* Perfil do usuário: `("/perfil")`
+* Atualizar perfil: `("/perfil/atualizar")`
+* Criar anúncio de carro: `("/publicar")`
+* Página individual do carro: `("/carros/:id")`
+* Página de atualização de informações do carro: `("/atualizar/:id")`
+* Página de listagem de carros: `("/carros?disponibilidade=&cidade=&precoMin=0&precoMax=10000000&ordem=")`
+  &nbsp;
+
+---
+## Como inicializar
+
+Primeiramente será necessário abrir o aplicativo `Docker Desktop`. Também será necessário parar outras imagens/conteineres pois além de ter um limite de espaço, a rota do localhost pode ser a mesma e acabar acontecendo um erro do aplicativo já estar aberto.
+
 ```
-docker run -d -p 8088:80 --name welcome-to-docker docker/welcome-to-docker
+docker ps -a
 ```
-And open `http://localhost:8088` in your browser.
 
-# Building
-
-Maintainers should see [MAINTAINERS.md](MAINTAINERS.md).
-
-Build and run:
 ```
-docker build -t welcome-to-docker . 
-docker run -d -p 8088:3000 --name welcome-to-docker to-docker
+docker stop (CONTAINER ID)
 ```
-Open `http://localhost:8088` in your browser.
+
+```
+docker rm (CONTAINER ID)
+```
+
+Por ser um arquivo com `Docker File`, a instalação com `npm install` não é necessária. Ao em vez disso, será feito o build e o run com os comando do Docker.
+
+```
+docker build -t garagelink .
+```
+
+Após a instalação, será feita a inicialização do arquivo Docker.
+
+```
+docker run -d -p 3001:3000 --name conteinergaragelink garagelink
+```
+
+Desse jeito, o projeto será iniciado em `http://localhost:3001`. Porém, antes de tentar qualquer outra coisa, será necessário iniciar o back-end com as instruções que serão dadas no README do projeto.
+
+```
+https://github.com/Diego-Sant/MVP-GarageLinkAPI-DesenvolvimentoBackEnd
+```
+  &nbsp;
+
+---
+## Fluxograma
+
+![Fluxograma feito no eraser.io](/public/readmeimage/fluxograma.png)
+  &nbsp;
+
+---
+## Sobre a API Externa
+
+O `Cloudinary` é utilizado no projeto para facilitar o upload, armazenamento, gerenciamento e otimização de imagens dos carros anunciados e da foto de perfil do usuário. Ele oferece uma solução completa de gerenciamento de mídia, permitindo que as imagens sejam hospedadas externamente e otimizadas automaticamente para o desempenho do site, sem a necessidade de ocupar espaço no servidor principal.
+
+Os arquivos que eu trato a api externa são o `uploadCarWidget.jsx` e o `uploadWidget.jsx`, o primeiro arquivo trata das imagens dos carros que serão enviadas na publicação, e o segundo trata da imagem do usuário.
+  &nbsp;
+
+---
+## Como fazer uma publicação
+
+Após criar e entrar com uma conta, você será direcionado ao menu principal, em seguida clique em "Perfil" na área de navegação e "Criar anúncio" do lado de "Minhas publicações". Para poupar tempo todas as informações estarão no arquivo `dummydata.js` no diretório `(/src/lib/dummydata.js)`.
--- src/lib/dummydata.js
+++ src/lib/dummydata.js
@@ -1,58 +1,39 @@
 export const listData = [
     {
-        title: "Chevrolet Onix 2020", // Chevrolet Onix 2020 é um exemplo
-        brand: ["Audi", "Bentley", "BMW", "BYD", "Chevrolet",
-        "Citroen", "Ferrari", "Fiat", "Ford", "Honda", "Hyundai",
-        "Jaguar", "Jeep", "Kia", "Lamborghini", "LandRover", "Maserati",
-        "McLaren", "MercedesBenz", "Mitsubishi", "Nissan",
-        "Peugeot", "Porsche", "Renault", "RollsRoyce",
-        "Suzuki", "Tesla", "Toyota", "Volkswagen", "Yamaha"],
-        condition: ["Novo", "Usado"],
-        transmission: ["Manual", "Automatico"],
-        buyOrRent: ["Alugar", "Comprar"],
-        priceToRent: 150, // Se for "Alugar" em buyOrRent, 150 é um exemplo
-        priceToBuy: 150000, // Se for "Comprar" em buyOrRent, 150000 é um exemplo
-        fuel: ["Flex", "Diesel", "Eletrecidade", "Gasolina", "Gas"],
-        color: ["Vermelho", "Amarelo", "Azul", "Bege", "Ciano", "Cinza", "Dourado",
-            "Laranja", "Marrom", "Prata", "Preto", "Purpura", "Rosa", "Roxo",
-            "Turquesa", "Verde"],
-        city: "Duque de Caxias", // Duque de Caxias é um exemplo é um exemplo, pode ser qualquer lugar do mundo(mas de preferência no Brasil)
-        address: "Duque de Caxias, RJ - Brasil", // Duque de Caxias, RJ - Brasil é um exemplo, pode ser qualquer lugar do mundo(mas de preferência no Brasil)
+        Images: "As imagens estarão no arquivo /public/readmeimage/image[].jpg",
+        Nome: "Lamborghini Aventador SVJ 2019",
+        ComprarOuAlugar: "Comprar",
+        Valor: 6900000,
+        Cidade: "Hollywood",
+        Endereço: "Hollywood, FL - EUA",
+        Condição: "Novo",
+        Marca: "Lamborghini",
+        Transmissão: "Automático",
+        Cor: "Verde",
+
+        Descrição: 
+        "O Lamborghini Aventador SVJ é um supercarro de alto desempenho com motor V12 de 6.5 litros, gerando 770 cv e 720 Nm de torque. Com aceleração de 0 a 100 km/h em 2,8 segundos e velocidade máxima acima de 350 km/h, o SVJ é conhecido por seu design aerodinâmico agressivo e exclusividade, oferecendo uma experiência de condução excepcional.",
+        
+        Combustível: "Gasolina",
+        Latitude: 26.01345383605297, 
+        Longitude: -80.15000947259898,
+
+        Titulo1: 
+        "Supercarro V12 Potente",
+
+        Descrição1: 
+        "770 cv, 0-100 km/h em 2,8s.",
+
+        Titulo2: 
+        "Performance Extremada",
+
+        Descrição2: 
+        "Velocidade máxima acima de 350 km/h.",
+
+        Titulo3: 
+        "Design Aerodinâmico",
+
+        Descrição3: 
+        "Linhas agressivas e tecnologia de ponta."
     },
 ]
-
-export const singlePostData = [
-    {
-        id: 1,
-        title: "Ford Fiesta 2018",
-        brand: "Ford",
-        images: [
-            ["https://diego-sant.github.io/QuizApp/img/img279.svg"],
-            "https://diego-sant.github.io/QuizApp/img/img280.svg",
-            ["https://diego-sant.github.io/QuizApp/img/img279.svg"],
-            ["https://diego-sant.github.io/QuizApp/img/img279.svg"],
-        ],
-        condition: "Usado",
-        transmission: "Manual",
-        buyOrRent: "Alugar",
-        priceToRent: 120,
-        fuel: "Flex",
-        color: "Prata",
-        address: "São Paulo, SP - Brasil",
-        description: "O Ford Fiesta 2018 é um hatchback compacto com um design moderno e elegante. Oferece um interior bem equipado com sistema de infotainment SYNC 3, compatível com Apple CarPlay e Android Auto, e um motor eficiente de 1.6 litro, disponível com transmissão manual de 5 marchas ou automática de 6 marchas. O Fiesta combina estilo, conforto e tecnologia, com recursos de segurança como controle de estabilidade e airbags para uma condução segura e agradável.",
-        general1Title: "Design Moderno",
-        general1Desc: "Visual arrojado e elegante.",
-        general2Title: "Tecnologia Avançada",
-        general2Desc: "Sistema SYNC 3 com Apple CarPlay e Android Auto.",
-        general3Title: "Desempenho Eficiente",
-        general3Desc: "Motor 1.6 litro com boas opções de transmissão.",
-        latitude: -23.59105941675351,
-        longitude: -46.69087039560111,
-    },
-];
-
-export const userData = {
-    id: 1,
-    name: "Diego",
-    images: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-}
