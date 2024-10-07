db7e2f63e60c6fd284fe3a23da4614889dfad76f
Diego de Souza Santana <diegossantana068@gmail.com>
2024-10-07 18:46:11 -0300

    searching fix

--- src/routes/listPage.jsx
+++ src/routes/listPage.jsx
@@ -16,10 +16,16 @@ function ListPage() {
                         <div className="flex justify-center items-center min-h-[200px]">
                             <div className="loader dark:loaderDark"></div>
                         </div>}>
-                        <Await resolve={data.postResponse} errorElement={<p>Erro ao carregar carros!</p>}>
-                            {(postResponse) => postResponse.data.map(post => (
-                                <Card key={post.id} item={post}/>
-                            ))}
+                        <Await resolve={data.postResponse} errorElement={<p className="text-center text-black dark:text-white text-lg mt-6">Erro ao carregar carros!</p>}>
+                            {(postResponse) => 
+                                postResponse.data.length > 0 ? (
+                                    postResponse.data.map(post => (
+                                        <Card key={post.id} item={post}/>
+                                    ))
+                                ) : (
+                                    <p className="text-center text-black dark:text-white text-lg mt-6">Não existe esse carro no nosso banco de dados com essas informações.</p>
+                                )
+                            }
                         </Await>
                     </Suspense>
                 </div>
@@ -29,7 +35,7 @@ function ListPage() {
                     <div className="flex justify-center items-center">
                         <div className="loader dark:loaderDark"></div>
                     </div>}>
-                    <Await resolve={data.postResponse} errorElement={<p>Erro ao carregar mapa!</p>}>
+                    <Await resolve={data.postResponse} errorElement={<p className="text-center text-black dark:text-white text-lg mt-6">Erro ao carregar mapa!</p>}>
                         {(postResponse) => <Map items={postResponse.data} />}
                     </Await>
                 </Suspense>
